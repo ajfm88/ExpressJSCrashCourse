af9ef18fe8db496f4fa28af60ac81b58768e575f
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-06 19:26:57 -0700

    03. REST API and Middleware

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
-// Set statuc folder
+// Init middleware
+app.use(logger);
+
+// Gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
